50fc15eecaeb18a328b9cedb7c55072f0fa06aaf
mama2940 <mushimo@live.com>
2014-10-02 20:09:14 +0200

    Added some ideas and thoughts to the code

--- grafbib.c
+++ grafbib.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node{
   int *busstop;
   int *time;
@@ -8,3 +11,28 @@ typedef struct arch{
   int *busnumber;
   int *traveltime;
 } *Arch;
+
+
+/* ett till argument? eftersom node tar strukten består av två*/
+Node nyNod (char *busstop) { 
+     Node newNode = malloc(sizeof(struct node));
+     newNode->busstop = malloc(strlen(busstop) + 1);
+     strcpy(newNode->busstop, busstop);
+     return newNode;
+}
+
+Arch nyArch() {
+}
+
+Node query() {
+}
+
+void destroyNode() {
+}
+
+void destroyArch() {
+}
+
+void insert () {
+}
+
