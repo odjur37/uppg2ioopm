64cbe82beadbd2c4f26630be9cee4b5f23804aef
mama2940 <mushimo@live.com>
2014-10-06 16:13:50 +0200

    Added istring_to_string function and fixed istring_mk compiling error starting from line 19. Everything compiles and runs but does not work as thought.

--- istring.c
+++ istring.c
@@ -4,22 +4,44 @@
 /*#define START 1
 #define STRING 2
 */
+
 int main(){
-char *istring_mk(const char* str) {
-if (str == NULL){
-return NULL;
- }
- char* istring = malloc(sizeof(int) + strlen(str));
+  char *istring_mk(const char* str) {
+    if (str == NULL){
+      return NULL;
+    }
+    
+char* istring = malloc(sizeof(int) + strlen(str));
  if (istring == NULL){
-   return NULL;
+   exit(1);
  }
+
  int length;
  length = strlen(str);
- return istring;
+ char string_length[length];
+ strcpy(string_length, (str + length));
+ printf("%c",string_length[2]);
+ return 0;
 }
  
- void istring_rm(char *str) {
-   free(str);
-   str = NULL;
+void istring_rm(char *str) {
+  free(str);
+  str = NULL;
+}
+ istring_mk("Hej");
+ return 0;
+ 
+char *istring_to_string(const char *str) {
+  int length = *(int *) (str - sizeof(int));
+  char *temp = malloc(length);
+  strncpy(temp, str, length);
+  int tempint;
+  tempint = strlen(temp);
+  temp[tempint+1] = '\0';
+  return temp;
  }
-  }
+
+ 
+}
+
+
